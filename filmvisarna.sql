0e1d7e153617ee28505b2cb983b7cbd8ec376e6f
Linus Almgren <linus.almgren@gmail.com>
2024-09-30 22:52:39 +0200

    create movie table

--- filmvisarna.sql
+++ filmvisarna.sql
@@ -2,3 +2,15 @@ CREATE TABLE genre (
     id int AUTO_INCREMENT PRIMARY KEY,
     genre_name varchar(30) NOT NULL
 );
+
+CREATE TABLE movie (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    title varchar(100) NOT NULL,
+    play_time int NOT NULL,
+    original_title varchar(100),
+    year_recorded date,
+    director varchar(50),
+    actors_cat varchar(255),
+    description text,
+    trailer_url varchar(100)
+);
