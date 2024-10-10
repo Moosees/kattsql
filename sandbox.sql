df01ee097caece2e680d63bd1cedb139b481c4a5
Linus Almgren <linus.almgren@gmail.com>
2024-10-10 22:17:32 +0200

    create procedure to create members and generate reservation_numbers

---
+++ sandbox.sql
@@ -0,0 +1,20 @@
+-- procedure to create a booking and a member if they don't exist
+DELIMITER //
+
+CREATE PROCEDURE create_reservation (IN email varchar(100), IN screeningId int, OUT reservationId char(6)) 
+BEGIN
+	DECLARE randomResNum CHAR(6) DEFAULT UPPER(SUBSTRING(TO_BASE64(MD5(RAND())), 1 + RAND(), 6));
+	DECLARE memberId int;
+
+	INSERT IGNORE INTO member (member_email) VALUES (email);
+
+	SELECT m.id INTO memberId FROM member m WHERE m.member_email = email;
+
+	INSERT INTO reservation (reservation_num, member_id, screening_id) VALUES
+	(randomResNum, memberId, screeningId);
+
+	SELECT r.id INTO reservationId FROM reservation r WHERE r.reservation_num = randomResNum;
+
+END //
+
+DELIMITER ;
