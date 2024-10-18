1f29a8ebf75010ea4f1d87e582505880382ca3fc
Linus Almgren <linus.almgren@gmail.com>
2024-10-18 14:14:36 +0200

    move create_reservation out data from procedure to select
    easier to work with in javascript probably

--- sandbox.sql
+++ sandbox.sql
@@ -1,7 +1,7 @@
 -- procedure to create a booking and a member if they don't exist
 DELIMITER //
 
-CREATE PROCEDURE create_reservation (IN email varchar(100), IN screeningId int unsigned, OUT reservationId int unsigned)
+CREATE PROCEDURE create_reservation (IN email varchar(100), IN screeningId int unsigned)
 BEGIN
 	DECLARE randomResNum CHAR(6) DEFAULT UPPER(SUBSTRING(TO_BASE64(MD5(RAND())), 1 + RAND(), 6));
 	DECLARE userId int;
@@ -13,7 +13,7 @@ BEGIN
 	INSERT INTO reservation (reservation_num, user_id, screening_id) VALUES
 	(randomResNum, userId, screeningId);
 
-	SELECT r.id INTO reservationId FROM reservation r WHERE r.reservation_num = randomResNum;
+	SELECT r.id as reservationId, randomResNum as reservationNum FROM reservation r WHERE r.reservation_num = randomResNum;
 
 END //
 
